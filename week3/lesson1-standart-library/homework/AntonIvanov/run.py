6442f44973f0f6af7ba1b2374b04ceec30bcfc00
Aersum <antoniosivanovs@gmail.com>
2018-10-11 00:32:04 +0300

    Task3 Solved

--- week3/lesson1-standart-library/homework/AntonIvanov/run.py
+++ week3/lesson1-standart-library/homework/AntonIvanov/run.py
@@ -1,9 +1,13 @@
 from utils.task1 import get_tree
 from utils.task2 import create_csv
 from utils.userlist import user_list
+from utils.task3 import get_image
 import os
 
 print("Task 1")
 get_tree(os.getcwd() + "/utils/tree")
 print("Task 2")
 create_csv(user_list, 'userlist.csv')
+print("Task3")
+url = "https://dummyimage.com/600x400/000/fff"
+get_image(url)
---
+++ week3/lesson1-standart-library/homework/AntonIvanov/utils/task3.py
@@ -0,0 +1,13 @@
+import requests
+
+def get_image(url: str):
+    file_name = 'task3.png'
+    res = requests.get(url)
+    if res.status_code == 200:
+        with open(file_name,'wb') as f:
+            f.write(res.content)
+    print(f'File {file_name} downloaded')
+
+if __name__ == "__main__":
+    url = "https://dummyimage.com/600x400/000/fff"
+    get_image(url)
