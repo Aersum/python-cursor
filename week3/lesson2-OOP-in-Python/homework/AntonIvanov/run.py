201c5755d2395cd94dc3a2b0b32a2802ff0038b5
Aersum <antoniosivanovs@gmail.com>
2018-10-13 21:10:53 +0300

    Task1 solved

--- week3/lesson2-OOP-in-Python/homework/AntonIvanov/run.py
+++ week3/lesson2-OOP-in-Python/homework/AntonIvanov/run.py
@@ -1,1 +1,23 @@
-# hw will be there
+from utils.task1 import *
+
+if __name__ == "__main__":
+    print(5*'*' + "Task 1" + 5*'*')
+    python_dev = PythonDeveloper("Anton", 2)
+    python_dev.about()
+    python_dev.write_code()
+    print(python_dev)
+    python_dev()
+
+    print ("*"*5)
+    java_dev = JavaDeveloper("Oleg", 7)
+    java_dev.about()
+    java_dev.write_code()
+    print(java_dev)
+    java_dev()
+
+    print("*" * 5)
+    ruby_dev = RubyDeveloper("Max", 4)
+    ruby_dev.about()
+    ruby_dev.write_code()
+    print(ruby_dev)
+    ruby_dev()
---
+++ week3/lesson2-OOP-in-Python/homework/AntonIvanov/utils/task1.py
@@ -0,0 +1,49 @@
+class Developer:
+    def __init__(self, name: str, years_experience: int):
+        self.name = name
+        self.years_experience = years_experience
+        self.language = ''
+
+    def __str__(self):
+        return f'{self.name} - {self.years_experience} years, {self.language}'
+
+    def about(self):
+        if self.years_experience <= 3:
+            print(f"My name is {self.name} and I am a Junior Developer.")
+        elif self.years_experience <= 5:
+            print(f"My name is {self.name} and I am a Middle Developer.")
+        else:
+            print(f"My name is {self.name} and I am a Senior Developer.")
+
+    def write_code(self):
+        print("I am a developer and I write code")
+
+    def __call__(self):
+        self.write_code()
+
+
+class PythonDeveloper(Developer):
+    def __init__(self, name: str, years_experience: int):
+        Developer.__init__(self, name, years_experience)
+        self.language = "Python"
+
+    def write_code(self):
+        print(f"I use {self.language} to write code")
+
+
+class JavaDeveloper(Developer):
+    def __init__(self, name: str, years_experience: int):
+        Developer.__init__(self, name, years_experience)
+        self.language = "Java"
+
+    def write_code(self):
+        print(f"I use {self.language} to write code")
+
+
+class RubyDeveloper(Developer):
+    def __init__(self, name: str, years_experience: int):
+        Developer.__init__(self, name, years_experience)
+        self.language = "Ruby"
+
+    def write_code(self):
+        print(f"I use {self.language} to write code")
